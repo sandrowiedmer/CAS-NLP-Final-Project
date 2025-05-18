09688a409f620535d2d345db1a08c3da1e3b44a4
sandrowiedmer <wiedmer.sandro@gmail.com>
2025-05-18 21:45:09 +0200

    Add files via upload

--- Code/data_acquisition.py
+++ Code/data_acquisition.py
@@ -11,6 +11,62 @@
 
 # existing IXM knowledge
 
+#multiple runs per model and prompt, as answers can vary from call to call, especially with “high” temperature
 
 #ollama list
-models =
+#model1='deepseek-r1:1.5b'
+#model2='gemma3:1b'
+#model3 = 'smollm2'
+#models = [model1, model2, model3]
+
+from prompt_collection import system_prompt, user_prompts
+from parameters import OLLAMA_HOST, models, temperatures
+
+print(system_prompt[0])
+
+def print_prompts_from_list():
+    for i, prompt in enumerate(user_prompts):
+        print(f"--- Prompt {i+1} ---")
+        print(prompt)
+
+if __name__ == "__main__":
+    print_prompts_from_list()
+
+print(models[0])
+print(models[1])
+print(models[2])
+
+# call all models with all prompts in list
+# save response each time in a file, prompt and model in the name
+
+
+### experimental
+from ollama import Client, GenerateResponse
+
+# You first request may take some time. The model has to be loaded into VRAM before
+# inference can begin. I've set a timeout of 300 seconds here.
+ollama = Client(OLLAMA_HOST, timeout=300)
+
+for model in models:
+    for prompt_id, prompt in enumerate(user_prompts):
+        for temperature in temperatures:
+            print(f"Generating for model '{model}', prompt {prompt_id}, temp {temperature}...")
+
+            response: GenerateResponse = ollama.generate(
+                model=model,
+                system=system_prompt,
+                prompt=prompt,
+                images=None,
+                stream=False,
+                options={
+                    "temperature": temperature,
+                    "num_ctx": 16384,
+                },
+            )
+# For a full explanation of the response, please see this URL:
+# https://github.com/ollama/ollama/blob/main/docs/api.md#examples
+            print(response.response)
+            print(response.json)
+
+            from save_response_module import save_response
+            save_response(model, prompt_id, temperature, response.response)
--- Code/prompt_collection.py
+++ Code/prompt_collection.py
@@ -1,20 +1,31 @@
+from parameters import MARKET, PRODUCTS
 
-# 1 For which steps in the semiconductor chain x-rays are used?
-# 2 Who is serving semi market? x-ray and not x-ray
-# 3 Where could in the future x-ray play a role (pains & gains)?
-# 4 Why x-ray is used, in which steps?
-# 5 What kind of other inspection technologies are used
+system_prompt = f"You are a helpful assistant with deep knowledge about the {MARKET} and corresponding manufacturing processes. Please help me do a market analysis for the {MARKET}, relevant for a company developing and offering {PRODUCTS}. Please answer in max. 100 words, with not to much stop words and no fillers. As much as possible only in bullet points."
 
-"""
-Please show me
-1. the steps of semiconductor manufacturing,
-2. in which of these x-rays are used and why
-2.a today and 2.b probably in the future
-which companies are serving the market
-which companies are buying x-eay solutions, today and potentially in the future?
-and 3. which other inspection technologies are used and at which steps.
-4. what else regarding semiconductor market could be from ineterest for a company developing and selling x-ray sources?
-"""
+
+
+#prompts list, ev. generic vs market specific prompts
+
+
+user_prompts_full_set = [
+    f"1: List all the steps of semiconductor manufacturing, with indication if it is back end BEOL or front end FEOL. 2: in which of these steps X-rays are used and why - 2.a) today and 2.b) potentially in the future",
+    f"Which companies are serving the semiconductor manufacturers with {PRODUCTS}, and which companies are serving the OEMS active in {MARKET}?",
+    f"Which other inspection technologies beside X-ray are used and at which steps of semiconductor manufacturing? For each of them: Why not X-ray: What are the pains of the current non-X-ray method and which advantages would using X-rays instead have?",
+    f"Tell me more about X-ray lithography XRL, X-ray diffraction XRD and X-ray fluorescence XRF.",
+    f"What else regarding the {MARKET} could be of interest for a company developing and selling {PRODUCTS}?",
+    f"Tell me about trends, relevant for us, what is the market potential of X-ray inspection in {MARKET}? What are the hurdles for entering the {MARKET}",
+    f"Who are you and which role are you playing?",
+]
+
+#4. Which companies active in the semiconductor market are buying X-ray sources, today and potentially in the future? Please give for each if it is a semiconductor manufacturer itself or an OEM serving the semiconductor manufacturer.
+
+
+# user_prompts = full_set indices, or:
+user_prompts = [
+    f"Tell me about trends, relevant for us, what is the market potential of X-ray inspection in {MARKET}? What are the hurdles for entering the {MARKET}",
+]
+
+#new: front end FEOL and back end BEOL
 
 # Zero Shot
 
